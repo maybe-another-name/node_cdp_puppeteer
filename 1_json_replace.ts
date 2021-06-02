4ebb98495318a0f629d9662a0cd1a7a1a1f1b26c
maybe-another-name <email.yet.another.one@gmail.com>
2021-06-02 10:05:54 -0400

    json replace values

--- 1_json_replace.ts
+++ 1_json_replace.ts
@@ -1,28 +1,71 @@
 import fs from "fs";
 
-const fileName:string = "sites/json_snippet.json";
+const fileName: string = "sites/json_snippet.json";
 
 let fileContents = fs.readFileSync(fileName, "utf-8");
 let mutableFileJsonObject: any = JSON.parse(fileContents);
 // console.log(mutableFileJsonObject);
 
-var matchRegex: RegExp = new RegExp(".*id.*|.*key.*|.*time.*|.*name.*|.*address.*", "i");
 var updateCountHolder = {
-  count: 0
+  count: 0,
 };
-replaceMatchingKeysInObj(
+
+const specialKeyword = "fabulous";
+// const longValueRegex:RegExp = /\S{16,}/gi;
+const longValueAndSpecialKeywordRegex: string = `\\S{16,}|.*${specialKeyword}.*`;
+
+const valueMatchRegex: RegExp = new RegExp(
+  longValueAndSpecialKeywordRegex,
+  "gi"
+);
+replaceMatchingValuesInObj(
   mutableFileJsonObject,
-  matchRegex,
+  valueMatchRegex,
   updateCountHolder
 );
-console.log(`updated ${updateCountHolder.count} entries`);
-var yeah_json = JSON.stringify(mutableFileJsonObject);
+console.log(
+  `updated ${updateCountHolder.count} long value and special entries`
+);
+
+updateCountHolder.count = 0;
+const keyMatchRegex: RegExp = /.*id.*|.*key.*|.*time.*|.*name.*|.*address.*/i;
+replaceMatchingKeysInObj(
+  mutableFileJsonObject,
+  keyMatchRegex,
+  updateCountHolder
+);
+console.log(`updated ${updateCountHolder.count} matching key entries`);
 // process.stdout.write(yeah_json+"\n");
-fs.writeFileSync(fileName+".1", yeah_json, "utf-8")
 
-console.log("complete");
+var yeah_json = JSON.stringify(mutableFileJsonObject);
+fs.writeFileSync(fileName + ".1", yeah_json, "utf-8");
+console.log("wrote updated file to disk");
 
+function replaceMatchingValuesInObj(
+  mutableObj: any,
+  regex: any,
+  updateCountHolder: any
+) {
+  if (typeof mutableObj != "object") {
+    return;
+  }
+  if (!mutableObj) {
+    return;
+  }
+  for (var child_key in mutableObj) {
+    let child_val: any = mutableObj[child_key];
+    if (typeof child_val == "string") {
+      if ((child_val as string).match(regex)) {
+        // console.log(`found a match for key: ${child_key}`);
+        mutableObj[child_key] = "blah";
+        updateCountHolder.count++;
+      }
+    }
+    replaceMatchingValuesInObj(mutableObj[child_key], regex, updateCountHolder);
+  }
 
+  return;
+}
 
 function replaceMatchingKeysInObj(
   mutableObj: any,
