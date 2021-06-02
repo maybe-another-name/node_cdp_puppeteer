b4f9cf5b23cb31c1e15068a3198830a249c8d2b3
maybe-another-name <email.yet.another.one@gmail.com>
2021-06-02 11:38:54 -0400

    waits and debugging

--- 2_playwright_site_testing.ts
+++ 2_playwright_site_testing.ts
@@ -1,4 +1,4 @@
-import * as site_data from "./sites/site_data"; 
+import * as site_data from "./sites/site_data";
 
 import { BrowserType, Page, Request, Route } from "playwright-core";
 
@@ -25,17 +25,35 @@ async function run_it() {
   setupRouting(page);
 
   console.log("navigating browser");
-  await page.goto(site_data.site_2);
-  await page.screenshot({ path: "example.png" });
+
+  interface PageOptions {
+    referer?: string;
+    timeout?: number;
+    waitUntil?: "load" | "domcontentloaded" | "networkidle";
+  }
+
+  const page_options: PageOptions = {
+    waitUntil: "domcontentloaded",
+  };
+
+  try {
+    await page.goto(site_data.site_2, page_options);
+    const selector= ".body-40"
+    await page.waitForSelector(selector);
+    console.log(`found selector ${selector}`);
+    await page.screenshot({ path: "example.png" });
+  } catch (error) {
+    console.error(error);
+  }
 
   // await browser.close();
-};
+}
 
 async function setupRouting(page: Page): Promise<void> {
   // file extensions are separate routes (additional suffixes would interfere)
   await page.route("**/*.{png,jpg,jpeg}", (route) => {
     let request = route.request();
-    console.log(`aborting request to  ${request.url()}`);
+    console.debug(`aborting request to  ${request.url()}`);
     route.abort();
   });
   // await page.route("**/*.{js}", (route) => {
@@ -44,11 +62,20 @@ async function setupRouting(page: Page): Promise<void> {
   //   route.abort();
   // });
 
+  // permitted pages
+  const admitted_pages: RegExp = /.*outlook.live.com.*/
+  await page.route(admitted_pages, (route) => {
+    let request = route.request();
+    console.debug(`continuing request to  ${request.url()}`);
+    route.continue();
+  });
+
+
   // Abort based on the request type
   await page.route("**/*", (route) => {
     let request = route.request();
 
-    // test based on content-typesz
+    // test based on content-types
     if (route_for_resource_type("image", route)) {
       return;
     }
@@ -56,21 +83,28 @@ async function setupRouting(page: Page): Promise<void> {
     //   return;
     // }
 
-    // default accept
-    console.log(`default proceed to host ${request.url()}`);
-    route.continue();
+    // default abort
+    console.debug(`default abort to host ${request.url()}`);
+    route.abort();
   });
 }
 
 function route_for_resource_type(resource_type: string, route: Route): boolean {
   let request = route.request();
   if (request.resourceType() === resource_type) {
-    console.log(`aborting ${resource_type} request to ${request.url()}`);
+    console.debug(`aborting ${resource_type} request to ${request.url()}`);
     route.abort();
     return true;
   }
   return false;
 }
 
+const DEBUG_MODE_ON = false;
+if (!DEBUG_MODE_ON) {
+  console = console || {};
+  console.debug = function () {};
+}
 
-(async () => { run_it() })();
+(async () => {
+  run_it();
+})();
